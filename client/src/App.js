58e4e8eb86f62baaccdef06308f7a33584430eca
Mario Lazzari <mario.lazzari@gmail.com>
2020-12-23 22:51:34 +0100

    Created project

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import Typography from "@material-ui/core/Typography";
+
+const App = () => {
+  return (
+    <div>
+      <Typography variant="h2">App</Typography>
+    </div>
+  );
+};
+
+export default App;
---
+++ server.js
@@ -0,0 +1,6 @@
+import express from "express";
+
+// Express server
+const app = express();
+
+app.listen(3001, () => console.log("mariolazzari.it started on port 3001."));
