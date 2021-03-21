e8aa8a02b1b7ae5fba0abcf3b105da3efcf5e8f4
Mario Lazzari <mario.lazzari@gmail.com>
2021-03-21 19:01:06 +0100

    Updated certifications

--- data/seeder.js
+++ data/seeder.js
@@ -9,24 +9,22 @@ const Skill = require("../models/Skill");
 const Social = require("../models/Social");
 
 // read json files
-//const jobs = JSON.parse(fs.readFileSync("./jobs.json", "utf-8"));
+const jobs = JSON.parse(fs.readFileSync("./jobs.json", "utf-8"));
 const nasaPods = JSON.parse(fs.readFileSync("./nasaPods2010.json", "utf-8"));
-//const skills = JSON.parse(fs.readFileSync("./skills.json", "utf-8"));
-//const socials = JSON.parse(fs.readFileSync("./socials.json", "utf-8"));
+const skills = JSON.parse(fs.readFileSync("./skills.json", "utf-8"));
+const socials = JSON.parse(fs.readFileSync("./socials.json", "utf-8"));
 
 // import data
 const importData = async () => {
   try {
     await NasaPicture.create(nasaPods);
     console.log("Nasa pods created");
-    /*
     await Job.create(jobs);
     console.log("Jobs imported.");
     await Skill.create(skills);
     console.log("Skills imported.");
     await Social.create(socials);
     console.log("Socials imported.");
-  */
   } catch (ex) {
     console.log("Error while importing data:", ex.message);
     process.exit(1);
@@ -36,16 +34,14 @@ const importData = async () => {
 // delete data from mongo db
 const deleteData = async () => {
   try {
-    /*
     await Job.deleteMany();
     console.log("Jobs deleted.");
     await Skill.deleteMany();
     console.log("Skills deleted.");
     await Social.deleteMany();
     console.log("Socials deleted.");
-  */
-    //    await NasaPicture.deleteMany();
-    //   console.log("Nasa pctures deleted.");
+    await NasaPicture.deleteMany();
+    console.log("Nasa pctures deleted.");
   } catch (ex) {
     console.log(ex.message);
     process.exit(1);
