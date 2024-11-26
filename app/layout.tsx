5eaac70991b5e3acda61220f96a093247e083f2a
Mario Lazzari <mario.lazzari@gmail.com>
2024-11-26 23:25:38 +0100

    feat: footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,15 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Footer } from "@/components/Footer";
 import { Layout } from "@/types/Layout";
+import { ThemeProvider } from "next-themes";
 
 export const metadata: Metadata = {
   title: {
     template: "%s | Mario Lazzari",
     default: "Mario Lazzari",
   },
-  description: "Mario Lazzari senior full stack developer",
+  description:
+    "Mario Lazzari senior full stack developer JavaScript TypeScript NodeJS ReactJS Go",
 };
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,11 +21,13 @@ function RootLayout({ children }: Layout) {
   return (
     <html lang="en" className={inter.className}>
       <body>
-        <Header />
-        <main className="h-[calc(100dvh-100px)] overflow-y-auto p-4">
-          {children}
-        </main>
-        <Footer />
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Header />
+          <main className="h-[calc(100dvh-100px)] overflow-y-auto p-4">
+            {children}
+          </main>
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,49 @@
+import Link from "next/link";
+import { Copyright } from "lucide-react";
+import {
+  FaLinkedin,
+  FaGithub,
+  FaInstagram,
+  FaFacebook,
+  FaTwitter,
+} from "react-icons/fa";
+
 export function Footer() {
+  const links = [
+    {
+      icon: <FaLinkedin />,
+      href: "https://www.linkedin.com/in/mario-lazzari",
+    },
+    {
+      icon: <FaGithub />,
+      href: "https://github.com/mariolazzari",
+    },
+    {
+      icon: <FaInstagram />,
+      href: "https://www.instagram.com/mario.v.lazzari",
+    },
+    {
+      icon: <FaFacebook />,
+      href: "https://www.facebook.com/mario.v.lazzari",
+    },
+    {
+      icon: <FaTwitter />,
+      href: "https://x.com/MarioLazzari2",
+    },
+  ];
+
   return (
-    <footer className="h-[50px] flex flex-col items-center bg-primary p-4">
-      Footer
+    <footer className="h-[50px] flex flex-col items-center bg-primary py-2">
+      <div className="flex gap-1">
+        {links.map(link => (
+          <Link key={link.href} href={link.href} target="_blank">
+            {link.icon}
+          </Link>
+        ))}
+      </div>
+      <p className="flex items-center gap-1 text-sm">
+        <Copyright size={16} /> Mario Lazzari {new Date().getFullYear()}
+      </p>
     </footer>
   );
 }
