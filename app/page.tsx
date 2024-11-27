92388601ce3fcb2e4939a0759892100499f576b7
Mario Lazzari <mario.lazzari@gmail.com>
2024-11-27 21:38:17 +0100

    refact: home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,12 @@ function HomePage() {
   return (
     <div className="flex flex-col items-center">
       <Logo />
-      <h1 className="text-6xl font-semibold text-primary text-center">
+      <h1 className="text-6xl font-bold text-primary text-center">
         Mario Lazzari
       </h1>
-      <h2 className="text-4xl text-center">Senior full stack developer</h2>
+      <h2 className="text-4xl text-center font-semibold">
+        Senior full stack developer
+      </h2>
 
       <div className="flex gap-2 my-4">
         <Button className="w-24">
@@ -20,6 +22,12 @@ function HomePage() {
           <Download /> CV
         </Button>
       </div>
+
+      <p className="max-w-xl text-center mb-4">
+        Benvenuti nel mio sito, nel quale potrete trovare alcune informazioni su
+        di me, i miei interessi e gli strumenti che utilizzo quotidianamente
+        come sviluppatore software.
+      </p>
     </div>
   );
 }
