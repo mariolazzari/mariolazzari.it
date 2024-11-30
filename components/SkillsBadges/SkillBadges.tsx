62b3735eb51787bbfab41d5d105f408f50990a60
Mario Lazzari <mario.lazzari@gmail.com>
2024-11-30 18:06:25 +0100

    refact: Hero component

--- components/SkillsBadges/SkillBadges.tsx
+++ components/SkillsBadges/SkillBadges.tsx
@@ -11,16 +11,18 @@ import { SkillBadgesProps } from "./SkillBadgesProps";
 
 export function SkillBadges({ title, skills }: SkillBadgesProps) {
   return (
-    <Card className="bg-gradient-to-br from-background to-muted">
+    <Card className="bg-gradient-to-br from-background to-muted border border-secondary group hover:border-primary">
       <CardHeader>
-        <CardTitle className="text-xl font-semibold">{title}</CardTitle>
+        <CardTitle className="text-xl font-semibold group-hover:text-primary">
+          {title}
+        </CardTitle>
         <CardDescription className="hidden"></CardDescription>
       </CardHeader>
       <CardContent className="w-[350px] h-[270px]">
         {skills.map(skill => (
           <Badge className="px-2 mr-2 my-1" key={skill.id} variant="secondary">
             <Link
-              className="flex items-center gap-2 text-lg"
+              className="flex items-center gap-2 text-lg hover:text-primary"
               href={skill.url}
               target="_blank"
             >
