e9fcb9a0c53207c638cd3b9deb1bc08e1cbffac3
Mario Lazzari <mario.lazzari@gmail.com>
2024-12-03 20:29:19 +0100

    feat: certifications images

--- components/Certifications/Certification.tsx
+++ components/Certifications/Certification.tsx
@@ -10,20 +10,27 @@ import { CertificationProps } from "./CertificationProps";
 import Link from "next/link";
 import { LinkIcon } from "lucide-react";
 import Image from "next/image";
+import { formatDistanceToNow } from "date-fns";
+import { it } from "date-fns/locale";
 
 export function Certification({ certification }: CertificationProps) {
   const { title, imagePath, date, url } = certification;
 
+  const renderDate = () => {
+    return formatDistanceToNow(date, { locale: it, addSuffix: true });
+  };
+
   return (
     <Link href={url} target="_blank">
-      <Card className="group">
-        <CardHeader>
-          <CardTitle className="group-hover:text-primary text-xl">
+      <Card className="group border border-muted hover:border-primary">
+        <CardHeader className="h-[100px]">
+          <CardTitle className="group-hover:text-primary text-xl ">
             {title}
           </CardTitle>
-          <CardDescription>{date}</CardDescription>
+          <CardDescription>{renderDate()}</CardDescription>
         </CardHeader>
-        <CardContent>
+
+        <CardContent className="h-[250px] flex items-center">
           <Image
             src={imagePath}
             alt={title}
@@ -32,7 +39,8 @@ export function Certification({ certification }: CertificationProps) {
             priority
           />
         </CardContent>
-        <CardFooter>
+
+        <CardFooter className="h-[50px]">
           <LinkIcon className="group-hover:text-primary group-hover:animate-pulse" />
         </CardFooter>
       </Card>
