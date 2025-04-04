23dc99d946ad84f6ac212e72e84294fde200b9cc
Mario Lazzari <mario.lazzari@gmail.com>
2025-04-04 08:48:13 +0200

    feat: certs

--- data/certifications.ts
+++ data/certifications.ts
@@ -2,6 +2,12 @@ import { Certification } from "@/types/Certification";
 
 export const certifications: Certification[] = [
   {
+    title: "JavaScript recursion",
+    date: "2025-04-04T09:00:00.000Z",
+    url: "https://www.linkedin.com/learning/certificates/daea56d766e932d740362939625f1506c558e0047e4908b098cec04dd204a39e?trk=share_certificate",
+    imagePath: "/certifications/js-recursion.png",
+  },
+  {
     title: "Level up JavaScript",
     date: "2025-03-31T22:00:00.000Z",
     url: "https://www.linkedin.com/learning/certificates/6480f470ec1cc4eafcdeb92a67496d2b20c1154c227a8fc4ec2844bd4fe26c92?trk=share_certificate",
