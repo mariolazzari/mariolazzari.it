34c3565f5e203acd4ab3d579886db756e888b728
Mario Lazzari <mario.lazzari@gmail.com>
2025-04-23 08:45:27 +0200

    updated certs

--- data/certifications.ts
+++ data/certifications.ts
@@ -2,6 +2,12 @@ import { Certification } from "@/types/Certification";
 
 export const certifications: Certification[] = [
   {
+    title: "Testing React with Vitest",
+    date: "2025-04-23T08:00:00.000Z",
+    url: "https://www.linkedin.com/learning/certificates/7788793e5fc14f7dbed465c0ca02dac40d9cb429d40cb45094e5b0f5a9531868?trk=share_certificate",
+    imagePath: "/certifications/react-vitest.png",
+  },
+  {
     title: "Improve JavaScript skills",
     date: "2025-04-15T20:00:00.000Z",
     url: "https://www.linkedin.com/learning/paths/improve-your-javascript-language-skills",
