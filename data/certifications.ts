086b886d4645f2324286ba0fc1f72b22f27cf14f
Mario Lazzari <mario.lazzari@gmail.com>
2026-05-01 18:19:28 +0200

    feat: certs

--- data/certifications.ts
+++ data/certifications.ts
@@ -2,6 +2,12 @@ import { Certification } from "@/types/Certification";
 
 export const certifications: Certification[] = [
   {
+    title: "HTTP client in Go",
+    date: "2026-05-01T08:00:00.000Z",
+    url: "https://www.udemy.com/certificate/UC-0ba8211d-8363-499e-aa81-b9364ae510e0/",
+    imagePath: "/certifications/go-http-client.png",
+  },
+  {
     title: "Working with React and Go",
     date: "2026-04-15T08:00:00.000Z",
     url: "https://www.udemy.com/certificate/UC-40c71835-f767-4038-96d6-e2bdc3dee993/",
