8df418a822809d258d939ead1f4c71be63e6ea4e
Mario Lazzari <mario.lazzari@gmail.com>
2026-06-21 09:42:38 +0200

    fix: docker locale button

--- frontend/components/Buttons/LocaleButton.tsx
+++ frontend/components/Buttons/LocaleButton.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import { usePathname, useRouter } from "@/i18n/routing";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
 import { useLocale } from "next-intl";
+import Image from "next/image";
+import itImg from "@/public/images/it.webp";
+import enImg from "@/public/images/en.png";
 
 export const LocaleButton = () => {
   const locale = useLocale();
   const router = useRouter();
   const pathname = usePathname();
+
+  const label = `Switch language (current: ${locale})`;
+  const src = locale === "it" ? itImg : enImg;
 
   const onClick = () => {
     const nextLocale = locale === "it" ? "en" : "it";
@@ -16,13 +22,13 @@ export const LocaleButton = () => {
 
   return (
     <Button
-      className="text-2xl rounded-full"
+      className="rounded-full cursor-pointer"
       variant="ghost"
       size="icon-lg"
+      aria-label={label}
       onClick={onClick}
-      aria-label={`Switch language (current: ${locale})`}
     >
-      {locale}
+      <Image src={src} alt={label} width={24} height={24} priority />
     </Button>
   );
 };
