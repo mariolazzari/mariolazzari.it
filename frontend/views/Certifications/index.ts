7c11e88be978b2c9e553ec87983c3e060a2d26a8
Mario Lazzari <mario.lazzari@gmail.com>
2026-09-10 17:00:49 +0200

    feat: certs

--- frontend/views/Certifications/index.ts
+++ frontend/views/Certifications/index.ts
@@ -6,6 +6,13 @@ export * from "./SearchResults";
 
 export const certifications: Certification[] = [
   {
+    title: "First personal project",
+    date: "2026-09-10T08:00:00.000Z",
+    url: "https://www.boot.dev/certificates/79b7c0c9-7bbe-49e6-aa99-c45044d13b1b",
+    imagePath: "/certifications/bd-first.png",
+    tags: ["go"],
+  },
+  {
     title: "Learn Git",
     date: "2026-09-05T08:00:00.000Z",
     url: "https://www.boot.dev/certificates/81b6e5d5-9cc6-417a-9050-470d8aadf6fd",
